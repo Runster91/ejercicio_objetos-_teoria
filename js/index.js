91386414a52d52e453f04c95636d9ab2f2c51850
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-07-15 12:11:00 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("hola mundo")
+
+const data = {
+  name: "Juan",
+  "real age": 25,
+  country: "MX"
+}
+
+// 1. DOT NOTATION
+console.log(data.name) // "Juan"
+
+// 2. BRACKET NOTATION
+console.log(data["real age"])
